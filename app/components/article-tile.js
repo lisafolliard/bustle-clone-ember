821f20f902bc9c39917e3f2492edcb2ef71e22b3
Janice Laset-Parkerson & Lisa Folliard <student@epicodus.com>
2015-09-22 15:27:19 -0700

    delete an article

--- app/components/article-tile.js
+++ app/components/article-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(article) {
+      if(confirm('You sure you want to delete?')) {
+        this.sendAction('destroyArticle', article);
+      }
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('articles');
   },
+
+  actions: {
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
